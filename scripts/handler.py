b2c4b5d84665137cbaf652701ffd48036882f679
Mike Bryant <mikesname@gmail.com>
2022-06-17 16:02:00 +0100

    Make handler use more env vars

--- scripts/handler.py
+++ scripts/handler.py
@@ -7,6 +7,9 @@ import sys
 
 secret = os.environ.get("SECRET", "")
 sync_dir = os.environ.get("SYNC_DIR")
+user = os.environ.get("GEOSERVER_USERNAME")
+workspace = os.environ.get("GEOSERVER_WORKSPACE")
+# NB GEOSERVER_PASSWORD should be in the env too
 if sync_dir is None or not os.path.isdir(sync_dir):
     raise Exception("Missing or invalid SYNC_DIR in environment")
 
@@ -21,6 +24,6 @@ else:
     print("Content-type: text/plain", end="\r\n\r\n", flush=True)
     os.chdir(sync_dir)
     os.system("git pull")
-    cmd = "geoserver-sync --user admin --workspace ehri --pattern '*.gpkg'"
+    cmd = f"geoserver-sync --user {user} --workspace {workspace} --pattern '*.gpkg'"
     if os.system(cmd) != 0:
         raise Exception(f"Command '{cmd}' exited with non-zero code")
